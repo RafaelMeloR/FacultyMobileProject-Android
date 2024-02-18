cc93195f667c4e21edbaeca12d4715827f0b1cab
Rafael Eduardo Melo Rodriguez <rafaeleduardomelorodriguez@gmail.com>
2024-02-17 23:12:51 -0500

    Feat: ALL_FACULTIES ACTIVITY AND RECICLE_VIEW

---
+++ FacultyMobileProject/app/src/main/java/com/example/facultymobileproject/AllFacultiesActivity.java
@@ -0,0 +1,14 @@
+package com.example.facultymobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllFacultiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_faculties);
+    }
+}
